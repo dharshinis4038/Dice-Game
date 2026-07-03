c0f279c261ccfc89c61cfedcbc4b515dfbaa52e7
dharshinis4038 <dharshinis4038@gmail.com>
2026-07-03 12:30:17 +0530

    Add initial implementation of Dice Game with HTML, CSS, and JavaScript

---
+++ script.js
@@ -0,0 +1,38 @@
+let dice = document.getElementById('dice');
+let rollDice = document.getElementById('roll-dice');
+let history = document.getElementById('history');
+let ul = document.querySelector('ul');
+let rollCount = 0
+
+function RollDice(){
+    let dot =  getCount();
+   dice.innerHTML=dot;
+   let li = document.createElement('li');
+   li.classList.add('roll');
+   li.innerHTML = `<div>Roll ${rollCount++}</div><div>${dot}</div>`;
+   ul.appendChild(li);
+}
+function getCount(){
+      let dot = Math.floor(Math.random()*6)+1;
+    switch(dot){
+    case 1:
+        return '&#9856';
+        break;
+    case 2:
+        return '&#9857';
+        break;
+    case 3:
+        return '&#9858';
+        break;
+    case 4:
+        return '&#9859';
+        break;
+    case 5:
+        return '&#9860';
+        break;
+    case 6:
+        return '&#9861';
+        break;    
+   }
+}
+rollDice.addEventListener('click',RollDice);
